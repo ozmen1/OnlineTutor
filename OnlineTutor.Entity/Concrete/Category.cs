7829c21ccb17c81b267bffb80e9048892440121e
Kerem Özmen <44169946+ozmen1@users.noreply.github.com>
2022-12-28 15:10:15 +0300

    katmanlar oluşturuldu

---
+++ OnlineTutor.Entity/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using OnlineTutor.Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineTutor.Entity.Concrete
+{
+    public class Category : IEntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
