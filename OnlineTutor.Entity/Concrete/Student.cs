1b85e0385d6f712e8a10ad606eccf039e91c0810
Kerem Özmen <44169946+ozmen1@users.noreply.github.com>
2023-01-13 14:44:30 +0300

    update

--- OnlineTutor.Entity/Concrete/Student.cs
+++ OnlineTutor.Entity/Concrete/Student.cs
@@ -11,6 +11,6 @@ namespace OnlineTutor.Entity.Concrete
     {
         public List<Comment>? Comments { get; set; }
         public List<Request>? Requests { get; set; }
-        public string Url { get; set; }
+        public string Url { get; set; } //comment
     }
 }
